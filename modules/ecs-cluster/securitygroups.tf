b2c7998831085571c742d631056d1a0c74bb2efd
Alyson Franklin <AlysonFranklinReal@gmail.com>
2020-01-25 18:06:15 -0300

    Adicionei o recurso aws_security_group_rule no modulo ecs-cluster para liberar a porta 443 para uso do VPC endpoint

--- modules/ecs-cluster/securitygroups.tf
+++ modules/ecs-cluster/securitygroups.tf
@@ -14,6 +14,16 @@ resource "aws_security_group_rule" "cluster-allow-ssh" {
   source_security_group_id = var.SSH_SG
 }
 
+resource "aws_security_group_rule" "vpc_endpoint" {
+  security_group_id        = aws_security_group.cluster.id
+  description              = "VPC Endpoint"
+  type                     = "ingress"
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.cluster.id
+}
+
 resource "aws_security_group_rule" "cluster-egress" {
   security_group_id = aws_security_group.cluster.id
   type              = "egress"
