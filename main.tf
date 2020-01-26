4e18ed6244bb3d5029bc3b4dabd5362a17c4f437
Alyson Franklin <AlysonFranklinReal@gmail.com>
2020-01-25 22:02:08 -0300

    Habilitei VPC endpoint completo para o ECS e ECR. Removi o bloco data source que buscava o securitygroup elo (não é mais necessário). Também formatei o código.

--- main.tf
+++ main.tf
@@ -11,12 +11,6 @@ terraform {
 data "aws_caller_identity" "current" {
 }
 
-/*
-data "aws_security_group" "selected" {
-  name = "ecs-elo7"
-}
-*/
-
 // Cria uma tabela no DynamoDB para lock do estado do terraform
 module "dynamodb" {
   source               = "./modules/dynamodb"
@@ -32,21 +26,27 @@ module "vpc" {
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  enable_nat_gateway                   = true  # Cria NAT Gateway nas subnets privadas
-  single_nat_gateway                   = false # Deve ser verdadeiro se você deseja provisionar UM ÚNICO NAT Gateway compartilhado em todas as subnets privadas
-  enable_vpn_gateway                   = false
-  enable_dns_hostnames                 = true
-  enable_s3_endpoint                   = true  # Habilita VPC Endpoint para S3
-  enable_sqs_endpoint                  = false # Habilita VPC Endpoint para SQS
-  enable_ecr_api_endpoint              = false # Habilita VPC Endpoint para ECR
-  enable_apigw_endpoint                = false # Habilita VPC Endpoint para API Gateway
-  ecr_dkr_endpoint_private_dns_enabled = false # Se deve ou não associar uma zona privada ao VPC Endpoint especificado para ECR DKR
+  enable_nat_gateway                         = true  # Cria NAT Gateway nas subnets privadas
+  single_nat_gateway                         = false # Deve ser verdadeiro se você deseja provisionar UM ÚNICO NAT Gateway compartilhado em todas as subnets privadas
+  enable_vpn_gateway                         = false
+  enable_dns_hostnames                       = true
+  enable_sqs_endpoint                        = false # Habilita VPC Endpoint para SQS
+  enable_ecr_api_endpoint                    = false # Habilita VPC Endpoint para ECR
+  enable_apigw_endpoint                      = false # Habilita VPC Endpoint para API Gateway
+  enable_s3_endpoint                         = true  # Habilita VPC Endpoint para S3
+  enable_ecr_dkr_endpoint                    = true  # Habilita VPC Endpoint para ECR dkr
+  enable_ecs_endpoint                        = true  # Habilita VPC Endpoint para ECS
+  enable_ecs_agent_endpoint                  = true  # Habilita VPC Endpoint para ECS Agent
+  enable_ecs_telemetry_endpoint              = true  # Habilita VPC Endpoint para ECS Telemetry
+  ecr_dkr_endpoint_private_dns_enabled       = true  # Se deve ou não associar uma zona privada ao VPC Endpoint especificado para ECR DKR
+  ecs_agent_endpoint_private_dns_enabled     = true  # Se deve ou não associar uma zona privada ao VPC Endpoint especificado para ECS Agent
+  ecs_telemetry_endpoint_private_dns_enabled = true  # Se deve ou não associar uma zona privada ao VPC Endpoint especificado para ECS Telemetry
+  ecs_endpoint_private_dns_enabled           = true  # Se deve ou não associar uma zona privada ao VPC Endpoint especificado para ECS Telemetry
 
-  //enable_ecr_dkr_endpoint             = true # Habilita VPC Endpoint para ECR dkr
-
-  // O ID de um ou mais grupos de segurança a serem associados à interface de rede para o VPC Endpoint ECR DKR
-  // É necessário 
-  //ecr_dkr_endpoint_security_group_ids = [data.aws_security_group.selected.id]
+  ecs_telemetry_endpoint_security_group_ids = [module.my-ecs.cluster_sg]
+  ecr_dkr_endpoint_security_group_ids       = [module.my-ecs.cluster_sg]
+  ecs_endpoint_security_group_ids           = [module.my-ecs.cluster_sg]
+  ecs_agent_endpoint_security_group_ids     = [module.my-ecs.cluster_sg]
 
   tags = {
     Terraform   = "true"
